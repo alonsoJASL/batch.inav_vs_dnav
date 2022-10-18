3a07a8bfeb9f478900a929569f6422d4655c1ced
Jose Alonso (@KCL) <alonso.jasl@gmail.com>
2022-10-18 16:59:05 +0100

    plots

--- nine_sweep_thresholds.m
+++ nine_sweep_thresholds.m
@@ -8,7 +8,7 @@ DIR = '/media/jsl19/sandisk/09-dnav_vs_inav/umc';
 subdirs={'local', 'docker'};
 N=sort([10 11 12 13 14 15 16 17 18 19 2  20 21 22 23 24 25 26 5  6  7  8  9]);
 
-sd=1;
+sd=1; % choose local
 
 resfolder= fullfile(DIR, ['results_' subdirs{sd}]);
 T=readtable(fullfile(resfolder,'stats.csv'));
@@ -28,10 +28,43 @@ end
 Td = T(contains(T.LGE_TYPE, 'dNAV'), :);
 Ti = T(contains(T.LGE_TYPE, 'iNAV'), :);
 
+
+
 repmat_fibscore_d = repmat(Td.FIB_SCORE', size(sweep_scores, 1), 1);
 [minvals, minidx] = min(abs(sweep_scores-repmat_fibscore_d));
 closest_inav_thres = thres_vect(minidx);
 
+%% FIGURES 
+figure(1)
+subplot(212)
+hold on
+for ix=1:length(Td.CASE)
+    plot([Td.CASE(ix) Td.CASE(ix)], [Td.FIB_SCORE(ix) Ti.FIB_SCORE(ix)], 'k:')
+end
+plot(Td.CASE, Td.FIB_SCORE, 'ob', Ti.CASE, Ti.FIB_SCORE, 'dr')
+xticks(Td.CASE)
+grid on
+title('FIBROSIS scores significantly different (p=0.011)', 'FontSize', 20)
+
+subplot(211)
+errorbar(Td.CASE, Td.MEAN_BP, Td.SDEV_BP, 'ob')
+hold on
+errorbar(Ti.CASE, Ti.MEAN_BP, Ti.SDEV_BP, 'dr')
+xticks(Td.CASE)
+grid on
+legend('dNAV', 'iNAV')
+title('MEAN Bloodpool not significantly different (p=0.347)', 'FontSize', 20)
+
+figure(3)
+subplot(121)
+boxplot(T.MEAN_BP, T.LGE_TYPE, 'Whisker', 1.2)
+title('MEAN Bloodpool not significantly different (p=0.347)', 'FontSize', 15)
+subplot(122)
+boxplot(T.FIB_SCORE, T.LGE_TYPE, 'Whisker', 1.2)
+title('FIBROSIS scores significantly different (p=0.011)', 'FontSize', 15)
+
+
+%% 
 function [thres_vect, scores_vect, m_bp, sd_bp] = getSweepResults(sweep_file)
 fi=fopen(sweep_file, 'r');
 
